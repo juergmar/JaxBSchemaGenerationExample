e7966c5022c2d9413050c27d8f0c5c1f9021edb7
oberstrike <markus.juergens@gmx.de>
2025-01-21 12:21:41 +0100

    Initial commit

---
+++ src/main/java/de/ma/form/jaxbschemagenerationexample/JaxBSchemaGenerationExampleApplication.java
@@ -0,0 +1,13 @@
+package de.ma.form.jaxbschemagenerationexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaxBSchemaGenerationExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JaxBSchemaGenerationExampleApplication.class, args);
+    }
+
+}
